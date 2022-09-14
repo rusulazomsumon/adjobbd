65c540d0991f1f4044de07c40f6a74e017692d47
Md. Rusul Azom <37295674+rusulazomsumon@users.noreply.github.com>
2022-09-14 15:17:04 +0600

    Update index.php
    update html

--- wp-content/themes/adjobsbd/index.php
+++ wp-content/themes/adjobsbd/index.php
@@ -1,13 +1,61 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>adjobsbd - Alternative Decents Jobs in Bangladesh.</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+    <!-- Bootstrap Font Icon CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <title>Exclusive job portal for the person with disabilits.
+    </title>
 </head>
+
 <body>
-    <h1>AdJobsBD</h1>
-    <p>Welcome!</p>
+    <div class="container-flued bg-img-header">
+        <div class="container-xxl">
+            <div class="row">
+                <div class="col ">
+                    <!-- site into and info -->
+                    <div class="siteIntro mt-5">
+                        <h1 class="text-center">ADJobsBD</h1>
+                        <p class="text-center">Exclusive Job Portal</p>
+                    </div>
+                </div>
+                <div class="row p-2 mb-5  bg-primary">
+                    <div class="col-2"></div>
+                    <div class="col-8 justify-content-center">
+                        <h3>Find Your Job</h3>
+                        <form action="">
+                            <input type="text" placeholder="Enter a Job Name">
+                            <select name="category" id="">
+                                <option value="Select">Select</option>
+                                <option value="IT Sector">IT Sector</option>
+                                <option value="Bangkinig">Bangkinig</option>
+                                <option value="others">Others</option>
+                            </select>
+                            <input type="text" placeholder="Enter a city, area or rigion">
+                            <input type="button" value="Search Job">
+                        </form>
+                    </div>
+                    <div class="col-2"></div>
+                </div>
+                <!-- 3rd Row Icons -->
+                
+            </div>
+        </div>
+    </div>
+
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
