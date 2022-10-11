13cbb263858a8f40b79af800d3af666fe42a22c9
RusulAzomSumon <18103020@iubat.edu>
2022-10-12 04:29:56 +0600

    Single Job post done

--- wp-content/themes/adjobsbd/single-job.php
+++ wp-content/themes/adjobsbd/single-job.php
@@ -11,16 +11,23 @@
                     <div class="col-lg-8">
                         <div class="d-flex align-items-center mb-5">
                             <?php 
-                                $author_id = $post->job; 
-                                $location = get_field('location', $author_id);
-                                $job_type = get_field('job_type', $author_id);
+                                $job_id = $post->job; 
+                                $location = get_field('location', $job_id);
+                                $job_type = get_field('job_type', $job_id);
+                                $vacency = get_field('vacency', $job_id);
+                                $job_category = get_field('job_category', $job_id);
+                                $deateline = get_field('deateline', $job_id);
+                                $sallery = get_field('sallery', $job_id);
+                                $company_logo = get_field('company_logo', $job_id);
+                                $company_title = get_field('company_title', $job_id);
+                                $company_description = get_field('company_description', $job_id);
                             ?>
                             <img class="flex-shrink-0 img-fluid border rounded" src="<?php the_post_thumbnail_url(); ?>" alt="" style="width: 80px; height: 80px;">
                             <div class="text-start ps-4">
                                 <h3 class="mb-3"><?php the_title(); ?></h3>
                                 <span class="text-truncate me-3"><i class="text-primary me-2"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/geo-alt-fill.svg"alt="file post"></i><?php echo $location; ?></span>
                                 <span class="text-truncate me-3"><i class="text-primary me-2"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/stopwatch.svg"alt="file post"></i><?php echo $job_type; ?></span>
-                                <span class="text-truncate me-0"><i class="text-primary me-2"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/cash.svg"alt="file post"></i>$123 - $456</span>
+                                <span class="text-truncate me-0"><i class="text-primary me-2"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/cash.svg"alt="file post"></i><?php echo $sallery ?> tk</span>
                             </div>
                         </div>
 
@@ -63,19 +70,20 @@
                     <!-- **************************** Left Sideber: Jobs Summery *********************************-->
 
                     <div class="col-lg-4">
-                        <div class="bg-light rounded p-5 mb-4 border border-primary wow slideInUp" data-wow-delay="0.1s">
+                        <div class="bg-light rounded p-5 mb-4 border border-primary wow slideInUp" data-wow-delay="0.1s">   
                             <h4 class="mb-4 text-center bg-primary">Job Summery</h4>
-                            <p><i class="text-primary me-2"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/cursor.svg"alt="adjobsbd"></i>Published On: <?php echo get_the_date() ?></p>
-                            <p><i class="text-primary me-2"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/cursor.svg"alt="adjobsbd"></i>Vacancy: 123 Position</p>
-                            <p><i class="text-primary me-2"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/cursor.svg"alt="adjobsbd"></i>Job Nature: Full Time</p>
-                            <p><i class="text-primary me-2"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/cursor.svg"alt="adjobsbd"></i>Salary: $123 - $456</p>
-                            <p><i class="ext-primary me-2"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/cursor.svg"alt="adjobsbd"></i>Location: New York, USA</p>
-                            <p class="m-0"><i class="text-primary me-2"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/cursor.svg"alt="adjobsbd"></i>Date Line: 01 Jan, 2045</p>
+                            <p><i class="text-primary me-2"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/cursor.svg"alt="adjobsbd"></i><b>Published On: </b><?php echo get_the_date() ?></p>
+                            <p><i class="text-primary me-2"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/cursor.svg"alt="adjobsbd"></i><b>Vacancy: </b><?php echo $vacency; ?></p>
+                            <p><i class="text-primary me-2"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/cursor.svg"alt="adjobsbd"></i><b>Job Nature: </b> <?php echo $job_type; ?></p>
+                            <p><i class="text-primary me-2"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/cursor.svg"alt="adjobsbd"></i><b>Salary: </b><?php echo $sallery; ?></p>
+                            <p><i class="ext-primary me-2"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/cursor.svg"alt="adjobsbd"></i><b>Location: </b><?php echo $location; ?></p>
+                            <p class="m-0"><i class="text-primary me-2"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/cursor.svg"alt="adjobsbd"></i><b>Date Line: </b><?php echo $deateline; ?></p>
                         </div>
                         <div class="bg-light rounded p-5 border border-primary wow slideInUp" data-wow-delay="0.1s">
                             <h4 class="mb-4 text-center bg-primary">Company Detail</h4>
-                            <img src="<?php the_post_thumbnail_url(); ?>" alt="" style="width: 80px; height: 80px; float: left; padding: 3px;">
-                            <p class="m-0">Ipsum dolor ipsum accusam stet et et diam dolores, sed rebum sadipscing elitr vero dolores. Lorem dolore elitr justo et no gubergren sadipscing, ipsum et takimata aliquyam et rebum est ipsum lorem diam. Et lorem magna eirmod est et et sanctus et, kasd clita labore.</p>
+                            <h5><?php echo $company_title; ?></h5>
+                            <img src="<?php echo $company_logo['url']; ?>" alt="" style="width: 80px; height: 80px; float: left; padding: 3px;">
+                            <p class="m-0"><?php echo $company_description; ?></p>
                         </div>
                     </div>
                 </div>
