91a3642beaafc975d140bf7c372148e299f56fdc
RusulAzomSumon <18103020@iubat.edu>
2022-10-15 02:35:39 +0600

    category by job incomplate

--- wp-content/themes/adjobsbd/category.php
+++ wp-content/themes/adjobsbd/category.php
@@ -11,67 +11,90 @@
             <div class="container">
                 <h1 class="text-center mb-5 wow fadeInUp" data-wow-delay="0.1s">Explore By Category</h1>
                 <div class="row g-4">
-                    <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
-                        <a class="cat-item rounded p-4" href="">
-                            <i class="fa fa-3x fa-mail-bulk text-primary mb-4"></i>
-                            <h6 class="mb-3">Marketing</h6>
-                            <p class="mb-0">123 Vacancy</p>
+                    <!-- showing all categor -->
+                    <!-- header socila linkin area, activate form ACM  -->
+                    <?php 
+                        
+                        $job_cat = get_field('job_categorics', $catid);
+                        
+                    
+                    ?>
+                    <!-- category content start --> 
+                    <div class="col-lg-12 col-sm-12 text-center wow fadeInUp" data-wow-delay="0.1s">
+                        <a class="cat-item rounded p-4" href="<?php echo $job_cat['category_link']; ?>">
+                            <i class="text-primary mb-4 p-5 "><img class="border border-primary rounded-circle shadow-sm" src="<?php echo $job_cat['category_icon']; ?>"alt="patch question" height="70px" width="70px"></i>
+                            <h6 class="mb-3"><?php echo $job_cat['category_title']; ?></h6>
+                            <p class="mb-0"><?php echo $job_cat['category_description']; ?></p>
                         </a>
                     </div>
-                    <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
-                        <a class="cat-item rounded p-4" href="">
-                            <i class="fa fa-3x fa-headset text-primary mb-4"></i>
-                            <h6 class="mb-3">Customer Service</h6>
-                            <p class="mb-0">123 Vacancy</p>
-                        </a>
-                    </div>
-                    <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
-                        <a class="cat-item rounded p-4" href="">
-                            <i class="fa fa-3x fa-user-tie text-primary mb-4"></i>
-                            <h6 class="mb-3">Human Resource</h6>
-                            <p class="mb-0">123 Vacancy</p>
-                        </a>
-                    </div>
-                    <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
-                        <a class="cat-item rounded p-4" href="">
-                            <i class="fa fa-3x fa-tasks text-primary mb-4"></i>
-                            <h6 class="mb-3">Project Management</h6>
-                            <p class="mb-0">123 Vacancy</p>
-                        </a>
-                    </div>
-                    <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
-                        <a class="cat-item rounded p-4" href="">
-                            <i class="fa fa-3x fa-chart-line text-primary mb-4"></i>
-                            <h6 class="mb-3">Business Development</h6>
-                            <p class="mb-0">123 Vacancy</p>
-                        </a>
-                    </div>
-                    <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.3s">
-                        <a class="cat-item rounded p-4" href="">
-                            <i class="fa fa-3x fa-hands-helping text-primary mb-4"></i>
-                            <h6 class="mb-3">Sales & Communication</h6>
-                            <p class="mb-0">123 Vacancy</p>
-                        </a>
-                    </div>
-                    <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.5s">
-                        <a class="cat-item rounded p-4" href="">
-                            <i class="fa fa-3x fa-book-reader text-primary mb-4"></i>
-                            <h6 class="mb-3">Teaching & Education</h6>
-                            <p class="mb-0">123 Vacancy</p>
-                        </a>
-                    </div>
-                    <div class="col-lg-3 col-sm-6 wow fadeInUp" data-wow-delay="0.7s">
-                        <a class="cat-item rounded p-4" href="">
-                            <i class="fa fa-3x fa-drafting-compass text-primary mb-4"></i>
-                            <h6 class="mb-3">Design & Creative</h6>
-                            <p class="mb-0">123 Vacancy</p>
-                        </a>
-                    </div>
+                    <!-- category content end -->
                 </div>
             </div>
         </div>
         <!-- Category End -->
 
+        <!-- Jobs Start -->
+        <div class="container-xxl py-5">
+            <div class="container">
+                <h1 class="text-center mb-5 wow fadeInUp" data-wow-delay="0.1s">Leatest Job Circuler</h1>
+                <div class="tab-class text-center wow fadeInUp" data-wow-delay="0.3s">
+                    <!-- single job -->
+                    <div class="tab-content">
+                        <div id="tab-1" class="tab-pane fade show p-0 active">
+                        <?php 
+                            $args = array(
+                                'post_type' =>  'job',
+                                'order' =>  'DSC',
+                                'posts_per_page' => 10
+                            );
+                            $query = new WP_Query($args);
+
+                            while($query -> have_posts()){
+                                $query -> the_post();
+                            ?>
+                            <!-- single post start -->
+                            <div class="job-item p-4 mb-4">
+                                <div class="row g-4">
+                                    <?php 
+                                        $job_id = $post->job; 
+                                        $location = get_field('location', $job_id);
+                                        $job_type = get_field('job_type', $job_id);
+                                        $deateline = get_field('deateline', $job_id);
+                                        $sallery = get_field('sallery', $job_id);
+                                        $company_logo = get_field('company_logo', $job_id);
+                                    ?>
+                                    <div class="col-sm-12 col-md-8 d-flex align-items-center">
+                                        <img class="flex-shrink-0 img-fluid border rounded" src="<?php echo $company_logo['url']; ?>" alt="" style="width: 80px; height: 80px;">
+                                        <div class="text-start ps-4">
+                                            <h5 class="mb-3"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                                            <span class="text-truncate me-3"><i class="text-primary me-2"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/geo-alt-fill.svg"alt="file post"></i><?php echo $location; ?></span>
+                                            <span class="text-truncate me-3"><i class="text-primary me-2"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/stopwatch.svg"alt="file post"></i><?php echo $job_type; ?></span>
+                                            <span class="text-truncate me-0"><i class="text-primary me-2"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/cash.svg"alt="file post"></i><?php echo $sallery; ?></span>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-12 col-md-4 d-flex flex-column align-items-start align-items-md-end justify-content-center">
+                                        <div class="d-flex mb-3">
+                                            <a class="btn btn-light btn-square me-3" href=""><i class="text-primary"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/heart.svg"alt="file post"></i></a>
+                                            <a class="btn btn-primary" href="">Apply Now</a>
+                                        </div>
+                                        <small class="text-truncate"><i class="text-primary me-2"><img src="<?php echo get_template_directory_uri(); ?>/assets/icons/cash.svg"alt="file post"></i>Date Line: <?php echo $deateline; ?></small>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- single page end -->
+                            <!-- end of loop -->
+                            <?php 
+                                }
+                                wp_reset_postdata();
+                            ?>
+                            <a class="btn btn-primary py-3 px-5" href="">Browse More Jobs</a>
+                        </div>
+                        <!--  job template end -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Jobs End -->
 
         <!-- Connecting Footer -->
         <?php get_footer(); ?>
