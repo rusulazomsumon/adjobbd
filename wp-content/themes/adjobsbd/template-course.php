a0631248cf40b7d348cd053792ff67164fc0ce84
RusulAzomSumon <18103020@iubat.edu>
2022-10-21 19:59:47 +0600

    course page added and updated

---
+++ wp-content/themes/adjobsbd/template-course.php
@@ -0,0 +1,26 @@
+<?php 
+            /* 
+            Template Name: Course Page 
+            */ 
+        ?>
+
+        <!-- Connecting Header -->
+        <?php get_header(); ?>
+
+        <!-- Course Page content -->
+        <div class="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
+            <div class="container text-center">
+                <div class="row justify-content-center">
+                    <div class="col-lg-6">
+                        <i class="bi bi-exclamation-triangle display-1 text-primary"></i>
+                        <h1 class="display-1">Our Course</h1>
+                        <p class="mb-4">Boots Your Career with our free and premium course</p>
+                        <a class="btn btn-primary py-3 px-5" href="">Enroll Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Course heading end -->
+
+        <!-- Connecting Footer -->
+        <?php get_footer(); ?>
